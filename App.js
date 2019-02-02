fc9ec1959c80c82563fff4b22f1d1c4a6b7a4ada
Lucas Andrade <lap.andrade15@gmail.com>
2019-02-02 01:51:23 -0300

    add react-navigation

--- App.js
+++ App.js
@@ -1,49 +1,25 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- * @lint-ignore-every XPLATJSCOPYRIGHT1
- */
+import {createStackNavigator, createAppContainer, styleSheet} from 'react-navigation'
+import LoginScreen from './src/pages/LoginScreen'
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-export default class App extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
+const AppNavigator = createStackNavigator({
+  Login: {
+    screen: LoginScreen,
+    navigationOptions: () => ({
+      title: 'Bem vindo!'
+    })
   }
+}, {
+  defaultNavigationOptions: {
+    title: 'Séries',
+    headerTintColor: '#fff',
+    headerStyle: {
+      backgroundColor: '#6ca2f7'
+    },
+    headerTitleStyle: {
+      color: '#fff',
+      fontSize: 25
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  }
 });
+
+export default createAppContainer(AppNavigator);
---
+++ src/pages/LoginScreen.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+export default class LoginScreen extends React.Component{
+    render() {
+        return(
+            <View> 
+                <Text> HAHA, ESTOU DE VOLTA, BB!</Text>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+
+});
